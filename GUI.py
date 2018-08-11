1039a252c99e7fa81ff40e362c50f06ce60b7cfc
sabSAThai <pranavsankhe40@gmail.com>
2018-08-12 04:20:20 +0530

    conv 2d

--- GUI.py
+++ GUI.py
@@ -1,6 +1,10 @@
 # Python Starter Code Author: Devdatta Kathale
 
-from PyQt4 import QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget,QFileDialog, QInputDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout, QGridLayout
+
+
 
 # from matplotlib.figure import Figure # Import matplotlib figure object
 # #from matplotlib.backends.backend import FigureCanvasQTAgg as FigureCanvas 
@@ -15,7 +19,7 @@ import cv2
 
 import sys
 import os
-from PyQt4.QtGui import *
+from PyQt5.QtGui import *
 
 from matplotlib.backends.qt_compat import QtCore, QtWidgets, is_pyqt5
  
@@ -24,6 +28,18 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 np.set_printoptions(threshold='nan')
+
+def conv2D(image,kernel):
+
+    ker_x = kernel.shape[0]
+    ker_y = kernel.shape[1]
+    img_x = image.shape[0]
+    img_y= image.shape[1]
+
+    print(img_x)
+    print(image[3][3])
+    image = cv2.copyMakeBorder(image, 2,2,2,2, cv2.BORDER_DEFAULT, value = 0)
+    print(image.shape)
 
 
 
@@ -61,7 +77,7 @@ def histogram_eq(image):
     return hist_eq_output_image
 
 
-class Window(QtGui.QWidget):
+class Window(QWidget):
     
 
     def __init__(self):
@@ -73,40 +89,49 @@ class Window(QtGui.QWidget):
         self.setWindowTitle("Basic IP Tool")
 
         #define grid for auto layout setting, this makes managing the window quite easy 
-        grid = QtGui.QGridLayout()
+        grid = QGridLayout()
         self.setLayout(grid)
 
         #defining button for loading an image
-        btn_load = QtGui.QPushButton("load_img", self)
+        btn_load = QPushButton("load_img", self)
         btn_load.clicked.connect(self.load_image)
         btn_load.resize(5,5)
         grid.addWidget(btn_load,0,0)
 
         #defining button for gamma correction
-        btn_gamma = QtGui.QPushButton("Gamma Correcn", self)
+        btn_gamma = QPushButton("Gamma Correcn", self)
         val_gamma = btn_gamma.clicked.connect(self.load_gamma)
         btn_gamma.resize(5,5)
         grid.addWidget(btn_gamma,0,1)
 
         #defining button for Log Transform
-        btn_log = QtGui.QPushButton("Log Transform", self)
+        btn_log = QPushButton("Log Transform", self)
         btn_log.clicked.connect(self.log_transform)
         btn_log.resize(5,5)
         grid.addWidget(btn_log,1,0)
 
         #defining button for histogram Equalization
-        btn_hist = QtGui.QPushButton("Histogram Eq", self)
+        btn_hist = QPushButton("Histogram Eq", self)
         btn_hist.clicked.connect(self.hist_eq)
         btn_hist.resize(5,5)
         grid.addWidget(btn_hist,1,1)
+
+        #defining button for convolution 2d
+        btn_blur = QPushButton("blur img", self)
+        btn_blur.clicked.connect(self.blur_img)
+        btn_blur.resize(5,5)
+        grid.addWidget(btn_blur,2,0)
 
 
         #define figure and canvas to plot the loaded image on this
         self.figure = plt.figure(figsize=(15,5))
         self.canvas = FigureCanvas(self.figure)
-        grid.addWidget(self.canvas,2,0,3,2)        
+        grid.addWidget(self.canvas,3,0,3,2)        
         self.show()
 
+    def blur_img(self):
+        kernel = (1/9)*np.matrix([[1, 1,1], [1, 1,1],[1,1,1]])
+        conv2D(original_image_gray,kernel)
 
     def load_image(self):
 
@@ -114,10 +139,12 @@ class Window(QtGui.QWidget):
 
         ax = self.figure.add_subplot(221)
         ax.set_title("Original Image")
-        filename = QtGui.QFileDialog.getOpenFileName(self,'select')
-        original_image_gray = cv2.imread(str(filename),0)
+        filename = QFileDialog.getOpenFileName(self,'select')
+        print(filename[0])
+        original_image_gray = cv2.imread(str(filename[0]),0)
         print(original_image_gray.shape)
-        print(original_image_gray)
+        # print(original_image_gray)
+        print("dfs")
         plt.imshow(original_image_gray, cmap = "gray")
         self.canvas.draw()
 
@@ -144,7 +171,7 @@ class Window(QtGui.QWidget):
             log_transformed_img = c_for_log*(np.log10(1 + original_image_gray))
             #need to convert the value to interger becuse log has decimal values
             log_transformed_img = log_transformed_img.astype(int)
-            print(log_transformed_img)
+            # print(log_transformed_img)
             ax = self.figure.add_subplot(223)
             ax.set_title("Log Transformed Image")
             plt.imshow(log_transformed_img, cmap = "gray")
@@ -176,7 +203,7 @@ class Window(QtGui.QWidget):
 
 
 def run():
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     GUI = Window()
     sys.exit(app.exec_())
 
