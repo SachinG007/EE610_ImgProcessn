2e9d785b037d1d3f531d8575e227dc98b75a8638
SachinG007 <sach.goyalsachin@gmail.com>
2018-08-21 05:43:47 +0530

    comments added

--- GUI.py
+++ GUI.py
@@ -136,12 +136,17 @@ class Window(QWidget):
 
         if ok:
             self.v_channel_prev = self.v_channel
+            # pass the v_channel to the gamma correction function    
             gamma_corr_v = gamma_correction(self.v_channel,gamma)
+            # since we are applying a power operation, we need to scale back all the values to 0-255
             gamma_max = gamma_corr_v.max()
             gamma_corr_v = gamma_corr_v * 255 /gamma_max
+            #update the v channel
             self.v_channel = gamma_corr_v[:,:]
+            #update teh v_channel in the hsv image
             self.hsv_image[:,:,2] = self.v_channel[:,:]
 
+            #convert the hsv image back to the RGB form for display    
             output = cv2.cvtColor(self.hsv_image,cv2.COLOR_HSV2BGR)
             ax = self.figure.add_subplot(222)
             ax.set_title("Gamma Corrected Image")
@@ -154,16 +159,21 @@ class Window(QWidget):
         c_for_log,ok = QInputDialog.getDouble(self,"Log Transformation","Value of C")
 
         if ok:
-   
+            
+            #maths equation   
             log_transformed_v = c_for_log*(np.log10(1 + self.v_channel))
             #need to convert the value to interger becuse log has decimal values
             log_transformed_v = log_transformed_v.astype(int)
+            #scale bacak to 0-255
             log_max = log_transformed_v.max()
             log_transformed_v = log_transformed_v * 255/log_max
+            #take care of the negative pixel values
             log_transformed_v = neg_pixel(log_transformed_v)
+            #add the cahnges to the hsv_image
             self.hsv_image[:,:,2] = log_transformed_v[:,:]
             self.v_channel = log_transformed_v[:,:]
 
+            #convert the hsv image back to the RGB form for display
             output = cv2.cvtColor(self.hsv_image,cv2.COLOR_HSV2BGR)
             ax = self.figure.add_subplot(222)
             ax.set_title("Log Transformed Image")
@@ -173,44 +183,54 @@ class Window(QWidget):
 
     def hist_eq(self):
 
+        #first displaying thr histogram of the original image
         ax = self.figure.add_subplot(222)
         ax.clear()
         plt.hist(self.v_channel.ravel(),256,[0,256]);
         ax.set_title("Histogram of Original Image")
 
+        #lets do the histogram equalization
         histogram_output_v = histogram_eq(self.v_channel)
+        
         hist_max = histogram_output_v.max()
         histogram_output_v = histogram_output_v * 255/hist_max
+        
         self.hsv_image[:,:,2] = histogram_output_v[:,:]
         self.v_channel = histogram_output_v[:,:]
 
-        # print(histogram_output_img)
+        #convert the hsv image back to the RGB form for display
         output = cv2.cvtColor(self.hsv_image,cv2.COLOR_HSV2BGR)
         ax = self.figure.add_subplot(224)
         ax.set_title("Histogram Equalization Output")
         plt.imshow(output)
         
+        #histogram of the new image for comparison
         ax = self.figure.add_subplot(223)
         ax.clear()
         plt.hist(self.v_channel.ravel(),256,[0,256]);
         ax.set_title("Histogram of New Image")
         # plt.show()
-
         self.canvas.draw()
 
 
     def blur_img(self):
 
+        #ask for the desired input size of the blurring kernel
         blur_size,ok = QInputDialog.getDouble(self,"Blur image","enter size of kernel (ODD ONLY")
-
+        #if input is done
         if ok:   
 
+            #convert input to integer
             blur_size= int(blur_size)
+
+            #construct the basic blurring filter of the given input size
             kernel = (1/np.power(blur_size,2))*np.ones((blur_size,blur_size))
+            #convol;ve the image with the blurring filter    
             blurred_v = conv2D(self.v_channel,kernel)
             blur_max = blurred_v.max()
             blurred_v = blurred_v * 255/blur_max
 
+            #resizing the output, in convolution I have not done padding of the image    
             x = self.v_channel.shape[0]
             y = self.v_channel.shape[1]
             blurred_v = cv2.resize(blurred_v,(y,x))
@@ -218,6 +238,7 @@ class Window(QWidget):
 
             self.hsv_image[:,:,2] = blurred_v[:,:]
             self.v_channel = blurred_v[:,:]
+            #convert the hsv image back to the RGB form for display   
             output = cv2.cvtColor(self.hsv_image,cv2.COLOR_HSV2BGR)
             ax = self.figure.add_subplot(224)
             ax.set_title("blurred Image")
@@ -226,16 +247,23 @@ class Window(QWidget):
 
     def gaussian_blur(self):
 
+        #implementation of the gaussian blurring
+        #ask for the kernel size
         kernel_size,ok = QInputDialog.getDouble(self,"Gaussian Blur image","enter size of kernel (ODD ONLY")
         if ok:   
 
+            #ask for the sigma value of the gaussian function
             sigma,ok = QInputDialog.getDouble(self,"Gaussian Blur image","enter vale of sigma")
             if ok:   
 
+                #convert the entered values to intergres
                 kernel_size= int(kernel_size)
                 sigma = int(sigma)
+
+                #generate the appropriate gaussian filter using the inputs provided
                 kernel = gen_gaussian(kernel_size,sigma)
-                print(kernel[2][2])
+                # print(kernel[2][2])
+                #do the convolution
                 blurred_v = conv2D(self.v_channel,kernel)
                 blur_max = blurred_v.max()
                 blurred_v = blurred_v * 255/blur_max
@@ -258,45 +286,54 @@ class Window(QWidget):
 
     def sharp_img(self):
 
+        #ask for the value of A
         A_hboost,ok = QInputDialog.getInt(self,"Sharp image","enter A>1 for high boost sharpening")
 
         if ok:         
 
+            #construct filter to compute the laplacian 
             kernel = np.matrix([[-1,-1,-1],[-1,A_hboost + 8,-1],[-1,-1,-1]])
             laplace_sharped_img = conv2D(self.v_channel,kernel)
             laplace_sharped_img = neg_pixel(laplace_sharped_img)
             
-
+            #sobel filter to calculate the first gradient in x direction
             kernel = np.matrix([[-1,-2,-1],[0,0,0],[1,2,1]])
             Gx = conv2D(self.v_channel,kernel)
             Gx = np.absolute(Gx)
 
+            #sobel filter to calculate the first gradient in y direction
             kernel = np.matrix([[-1,0,1],[-2,0,2],[-1,0,1]])
             Gy = conv2D(self.v_channel,kernel)
             Gy = np.absolute(Gy)
 
+            #add the gradients
             gradient = Gx + Gy
 
-            # kernel = (1/9)*np.matrix([[1,1,1],[1,1,1],[1,1,1]])
-            # smooth_gradient = conv2D(gradient,kernel)
+            #the sum of the sobel gradients needs to be blurred as discussed in the lecture
+            kernel = (1/9)*np.matrix([[1,1,1],[1,1,1],[1,1,1]])
+            smooth_gradient = conv2D(gradient,kernel)
             smooth_gradient = gradient
 
+            #resize the smooth gradient we computed to the original size so that we can multipky it with laplcian gradient image0
             x = laplace_sharped_img.shape[0]
             y = laplace_sharped_img.shape[1]
             smooth_gradient = cv2.resize(smooth_gradient,(y,x))
             max_smooth = smooth_gradient.max()
             smooth_gradient = smooth_gradient * 255/max_smooth
 
+            #compute the final mask by multipling the 2 images
             sharp_mask = np.multiply(smooth_gradient,laplace_sharped_img)
             max_mask = sharp_mask.max()
             sharp_mask = sharp_mask * 255/max_mask
 
-            
+            #resize the msk to the input image shape
             x = self.v_channel.shape[0]
             y = self.v_channel.shape[1]
             sharp_mask = cv2.resize(sharp_mask,(y,x))   
 
+            #add the mask and the input image
             output_sharp = self.v_channel + sharp_mask
+            #apply power law transforms for more bttr results    
             output_sharp_transformed = gamma_correction(output_sharp,0.6)
             out_max = output_sharp_transformed.max()
             output_sharp_transformed = output_sharp_transformed * 255/out_max
--- GUI_functions.py
+++ GUI_functions.py
@@ -16,8 +16,11 @@ import matplotlib.pyplot as plt
 
 def conv2D(image,kernel):
 
+    #flip the kernel so that convolution can be done
     kernel = np.flip(kernel,0)
     kernel = np.flip(kernel,1)
+
+    #computing the sizes of the kernel
     ker_x = kernel.shape[0]
     ker_y = kernel.shape[1]
     kerx_by2 = (np.floor(ker_x/2)).astype(int)
@@ -25,13 +28,16 @@ def conv2D(image,kernel):
 
     # print("input image shape")
     # print(image.shape)
-
+    #vectorizing the algorithm for fast implementation
+    #hence iflattened the kernel to a vector 
     kernel_vector = np.transpose(np.reshape(kernel,(kernel.size,1)))
 
     img_width = image.shape[0]
     img_height = image.shape[1]
 
-    #output will be smallr in size
+    #padding the image by a fixed size of 1 
+    #willlead to smaller size output 
+    #but seems to me better then having outputs of same size but black border
     conv_image = np.zeros((img_width - ker_x + 1,img_height - ker_y + 1))
 
     # print(image[3][3])
@@ -39,12 +45,14 @@ def conv2D(image,kernel):
     # pad_image = cv2.copyMakeBorder(image, 0,0,0,0, cv2.BORDER_DEFAULT, value = 0)
     pad_image = image
 
-    print("pad_img shape : " )
-    print(pad_image.shape)
+    # print("pad_img shape : " )
+    # print(pad_image.shape)
 
+    #now we run our kernel over each of the pixel of the input image to calculate the output
     for i in range(0 + kerx_by2 , img_width - kerx_by2 ):
         for j in range(0 + kery_by2, img_height - kery_by2 ):
 
+            #extract the patch of the input image overlapping the kernel in its curent position
             current_patch = pad_image[i-kerx_by2 : i+kerx_by2+1 , j-kery_by2 : j+kery_by2+1]
             current_patch_vector = np.reshape(current_patch,(kernel.size,1))
             # print(current_patch_vector.shape)
@@ -61,26 +69,33 @@ def gamma_correction(image,val_gamma):
 def histogram_eq(image):
 
     #first compute the frequncies of each of the intensity levels
+    #made a list size 256 for storing the number of times each intensity value occured
+    #index of list corresponds to the value of intensity
     intensity_freq = np.zeros((256,1))
 
+    #filling up the intensity freq list 
     for i in range(0,image.shape[0]):
         for j in range(0,image.shape[1]):
             intensity_val = image[i][j]
             intensity_freq[intensity_val] = intensity_freq[intensity_val] + 1;
 
+    #calculating teh cumulatove frequcies as required by the algoirthm
     cumulative_freq = np.zeros((256,1))
     sum =0
     for k in range(0,256):
         sum = sum + intensity_freq[k]
         cumulative_freq[k] = sum
 
+    #calculating the total pixels in the image
     total_pixels = image.shape[0] * image.shape[1]
 
+    #hence calculating the probabilities
     cumulative_freq_norm = 255*cumulative_freq/total_pixels    
 
     #construct the final image
     hist_eq_output_image = np.zeros((image.shape[0],image.shape[1]))
 
+    #assigning values to the final image matrix as per the algorithm
     for p in range(0,image.shape[0]):
         for q in range(0,image.shape[1]):
             hist_eq_output_image[p][q] = cumulative_freq_norm[image[p][q]]
@@ -91,6 +106,7 @@ def histogram_eq(image):
 
 def neg_pixel(image):
 
+    #function made to take care of teh issues when image has negative pixel values
     min_intensity = image.min()
     image = image - min_intensity
     max_intensity = image.max()
@@ -102,19 +118,27 @@ def neg_pixel(image):
 
 def gen_gaussian(kernel_size,sigma):
 
+    #function to geenrate teh gaussian amtrix
     kernel = np.zeros((kernel_size,kernel_size))
     kernel_sizeby2 = kernel_size/2
+    #sqared of sigma
     sigma2 = np.square(sigma)
 
+    #computing the gaussian values for each index position in the kernel
     for i in range(kernel_size):
         for j in range(kernel_size):
 
+            #x^2 + y^2
             kernel[i][j] = np.square(i - kernel_sizeby2) + np.square(j - kernel_sizeby2)
+            # -(x^2 + y^2)
             kernel[i][j] = -kernel[i][j]
+            #divide by sigma squarea
             kernel[i][j] = np.exp(kernel[i][j]/(2*sigma2))
 
+    #divide by 2pisigma sqaurea        
     kernel = kernel/(2*3.14*sigma2)
 
+    #normalize the kernel
     sum_all = np.sum(kernel)
     kernel = kernel/sum_all
 
